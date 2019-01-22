5b7e3bd92827eb3dd77afa990870251f1aab1343
jasond-qa <jason.derry@academytrainee.com>
2019-01-22 17:29:40 +0000

    more endpoints + amended database

--- src/main/java/com/qa/rest/ChamberEndpoint.java
+++ src/main/java/com/qa/rest/ChamberEndpoint.java
@@ -2,10 +2,12 @@ package com.qa.rest;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
+import com.qa.business.service.AccountService;
 import com.qa.business.service.WeaponService;
 
 @Path("/chamber")
@@ -14,13 +16,30 @@ public class ChamberEndpoint {
 	@Inject
 	private WeaponService wService;
 	
+	@Inject
+	private AccountService aService;
+	
 	@Path("/weapons")
 	@GET
 	@Produces({ "application/json" })
 	public String getWeapons(@QueryParam("type") String type) {
 		return type != null ? wService.getWeapons(type)
                 			: wService.getWeapons();
-	}	
+	}
+	
+	@Path("/getAllAccounts")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllAccounts() {
+		return aService.getAllAccounts();
+	}
+	
+	@Path("/createAccount")
+	@POST
+	@Produces({ "application/json" })
+	public String addAccount(String account) {
+		return aService.addAccount(account);
+	}
 	
 	public void setService(WeaponService service) {
 		this.wService = service;
